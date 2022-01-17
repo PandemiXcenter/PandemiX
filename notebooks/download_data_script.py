d07858989c55108c8a0a3a9b88b07fe9c953a01f
Rasmus Kristoffer Pedersen <rasmuspedersen1992@gmail.com>
2022-01-17 12:26:10 +0100

    Updated download script to correctly download all data going back to 2021-04-19, with a flag to only download most recent

--- notebooks/download_data_script.py
+++ notebooks/download_data_script.py
@@ -1,5 +1,5 @@
 # %%
-# Notebook for downloading all data
+# Script for downloading all data
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
@@ -16,7 +16,11 @@ import datetime as dt
 # import pycountry as pc
 import math
 
-
+# ----------------------- FLAG FOR DOWNLOADING ALL DATA -----------------------
+download_all_data = False
+# download_all_data = True
+print(f'Download all data: {download_all_data}')
+# -----------------------------------------------------------------------------
 
 # %%
 # Define paths
@@ -26,71 +30,58 @@ path_data = rootdir_data + "ssi_data"
 path_dash = rootdir_data + "ssi_dashboard"
 path_vacc = rootdir_data + "ssi_vacc"
 
-# path_figs = os.getcwd() +"\\..\\Figures\\" 
-
-
-# ssidatapath = "ssi_data"
-# currootdir = os.getcwd() +"/" + ssidatapath
 currootdir = path_data
 
 # %%
 prevDownloads = os.listdir(currootdir)
 mostRecent = prevDownloads[-1]
-print(mostRecent)
-# curDate = np.datetime64(mostRecent[-2:] + '-' + mostRecent[-5:-3] + '-' + mostRecent[-10:-6])
+# print(mostRecent)
 mostRecentDate = np.datetime64(mostRecent[-10:])
 mostRecentDate 
-print(mostRecentDate)
-
-# # Overvågningsdata
+print(f'Most recent data file: {mostRecent}, from date {mostRecentDate}')
 
 # %%
-get_data = True
-# ssidatapath = "ssi_data"
-# rootdir = os.getcwd() +"/" + ssidatapath
-# rootdir = path_data
-
 def download_url(url, save_path, chunk_size=128):
     r = requests.get(url, stream=True)
     with open(save_path, 'wb') as fd:
         for chunk in r.iter_content(chunk_size=chunk_size):
             fd.write(chunk)
 
+# %%
+### Overvågningsdata
 url = "https://covid19.ssi.dk/overvagningsdata/download-fil-med-overvaagningdata"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 links = soup.find_all("a", string=lambda text: "data" in str(text).lower())
 
-check_str = "<a href=\"https://files.ssi"
+# check_str = "<a href=\"https://files.ssi"
+check_str_over = "<a href=\"https://files.ssi.dk/covid19/overvagning/data/overvaagningsdata"
 for link in links[3:]: 
     # print('---')
-    #print(link)
-    if str(link)[:len(check_str)]!=check_str:
+    # print(link)
+    if str(link)[:len(check_str_over)]!=check_str_over:
         # print("not a file; continues...")
         continue
     # print(link)
     file = link["href"]
-    # yearPos = file.find('2021')
-    yearPos = file.find('2022-')
-    
-    if yearPos == -1:
-        # print("2021 not found in link; continues...")
-        continue
     # print(file)
-    curDate = file[yearPos:yearPos+4] + '-' + file[yearPos-2:yearPos] + '-' + file[yearPos-4:yearPos-2] 
+
+    # For all files after 2021-04-19, "-covid19-" is included in link
+    # To download earlier files, a different script should be made, in which something else is checked for in link...
+    covid19Pos = file.find('-covid19-')
+    curDay = file[covid19Pos+9:covid19Pos+11]
+    curMonth = file[covid19Pos+11:covid19Pos+13]
+    curYear = file[covid19Pos+13:covid19Pos+17]
+    curDate = f'{curYear}-{curMonth}-{curDay}'
+    curDateTime = np.datetime64(curDate)
+    # print(curDateTime)
     
-    # print(file)
-    
-    # Only download new data
-    curDatetime = np.datetime64(curDate)
-    if (curDatetime > mostRecentDate):
-        print(curDatetime)
+    if download_all_data:
 
         filename = "\\SSI_data_" + curDate
         zipped_save_path = path_data + "_zipped" + filename + ".zip"
         extracted_save_path = path_data  + filename
-        print(extracted_save_path)
-
+        print(f'Downloading: {extracted_save_path}')
         
         try:
             download_url(file, zipped_save_path)
@@ -98,113 +89,58 @@ for link in links[3:]:
                 zipObj.extractall(extracted_save_path)
         except: 
             print(file)
+    else:
+        if (curDateTime > mostRecentDate):
+            
+            filename = "\\SSI_data_" + curDate
+            zipped_save_path = path_data + "_zipped" + filename + ".zip"
+            extracted_save_path = path_data  + filename
+            print(f'Downloading: {extracted_save_path}')
+            
+            try:
+                download_url(file, zipped_save_path)
+                with zipfile.ZipFile(zipped_save_path, 'r') as zipObj:
+                    zipObj.extractall(extracted_save_path)
+            except: 
+                print('Error downloading... Filename:')
+                print(file)
 
-# %% [markdown]
-# # Vaccinedata
-
-# %%
-get_data = True
-# ssivaccpath = "ssi_vacc"
-# rootdir = os.getcwd() +"/" + ssivaccpath
-# rootdir = path_vacc
-
-def download_url(url, save_path, chunk_size=128):
-    r = requests.get(url, stream=True)
-    with open(save_path, 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=chunk_size):
-            fd.write(chunk)
-
-# def get_all_data():
-url = "https://covid19.ssi.dk/overvagningsdata/download-fil-med-vaccinationsdata"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-links = soup.find_all("a", string=lambda text: "data" in str(text).lower())
-# print(links)
-check_str = "<a href=\"https://files.ssi"
-for link in links[3:]: 
-    # print('---')
-    #print(link)
-    if str(link)[:len(check_str)]!=check_str:
-        # print("not a file; continues...")
-        continue
-    # print(link)
-    file = link["href"]
-    # yearPos = file.find('2021')
-    yearPos = file.find('2022-')
-    
-    if yearPos == -1:
-        # print("2021 not found in link; continues...")
-        continue
-    # print(yearPos)
-    # print(file[yearPos-4:yearPos+4])
-    curDate = file[yearPos:yearPos+4] + '-' + file[yearPos-2:yearPos] + '-' + file[yearPos-4:yearPos-2] 
-
-
-    # Only download new data
-    curDatetime = np.datetime64(curDate)
-    if (curDatetime > mostRecentDate):
-        print(curDatetime)
-
-
-        filename = "\\SSI_vacc_" + curDate
-        zipped_save_path = path_vacc + "_zipped" + filename + ".zip"
-        extracted_save_path = path_vacc + filename
-        print(extracted_save_path)
-        
-        try:
-            download_url(file, zipped_save_path)
-            with zipfile.ZipFile(zipped_save_path, 'r') as zipObj:
-                zipObj.extractall(extracted_save_path)
-        except: 
-            print(file)
-
-# %% [markdown]
-# # Dashboard data
+print('Done downloading "overvågningsdata" files')
 
 # %%
-get_data = True
-# ssidashpath = "ssi_dashboard"
-# rootdir = os.getcwd() +"/" + ssidashpath
-rootdir = path_dash
-
-def download_url(url, save_path, chunk_size=128):
-    r = requests.get(url, stream=True)
-    with open(save_path, 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=chunk_size):
-            fd.write(chunk)
-
-url = "https://covid19.ssi.dk/overvagningsdata/download-fil-med-overvaagningdata"
-
+### Dashboard data
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 # links = soup.find_all("a", string=lambda text: "data" in str(text).lower())
 links = soup.find_all("a", string=lambda text: "dash" in str(text).lower())
-check_str = "<a href=\"https://files.ssi"
-for link in links[1:]: 
-    
-
-    if str(link)[:len(check_str)]!=check_str:
+# check_str = "<a href=\"https://files.ssi"
+check_str_dash = "<a href=\"https://files.ssi.dk/covid19/overvagning/dashboard/overvaagningsdata-dashboard-"
+for link in links[3:]: 
+    # print('---')
+    # print(link)
+    if str(link)[:len(check_str_dash)]!=check_str_dash:
         # print("not a file; continues...")
         continue
+    # print(link)
     file = link["href"]
-    # yearPos = file.find('2021')
-    yearPos = file.find('2022-')
-    
-    if yearPos == -1:
-        # print("2021 not found in link; continues...")
-        continue
-    # print(yearPos)
-    # print(file[yearPos-4:yearPos+4])
-    curDate = file[yearPos:yearPos+4] + '-' + file[yearPos-2:yearPos] + '-' + file[yearPos-4:yearPos-2] 
+    # print(file)
 
-    # Only download new data
-    curDatetime = np.datetime64(curDate)
-    if (curDatetime > mostRecentDate):
-        print(curDatetime)
+    # For all files after 2021-04-19, "-covid19-" is included in link
+    # To download earlier files, a different script should be made, in which something else is checked for in link...
+    covid19Pos = file.find('-covid19-')
+    curDay = file[covid19Pos+9:covid19Pos+11]
+    curMonth = file[covid19Pos+11:covid19Pos+13]
+    curYear = file[covid19Pos+13:covid19Pos+17]
+    curDate = f'{curYear}-{curMonth}-{curDay}'
+    curDateTime = np.datetime64(curDate)
+    # print(curDateTime)
+    
+    if download_all_data:
 
         filename = "\\SSI_dashboard_" + curDate
         zipped_save_path = path_dash + "_zipped" + filename + ".zip"
         extracted_save_path = path_dash + filename
+        print(f'Downloading: {extracted_save_path}')
         
         try:
             download_url(file, zipped_save_path)
@@ -212,5 +148,80 @@ for link in links[1:]:
                 zipObj.extractall(extracted_save_path)
         except: 
             print(file)
+    else:
+        if (curDateTime > mostRecentDate):
+            
+            filename = "\\SSI_dashboard_" + curDate
+            zipped_save_path = path_dash + "_zipped" + filename + ".zip"
+            extracted_save_path = path_dash + filename
+            print(f'Downloading: {extracted_save_path}')
+            
+            try:
+                download_url(file, zipped_save_path)
+                with zipfile.ZipFile(zipped_save_path, 'r') as zipObj:
+                    zipObj.extractall(extracted_save_path)
+            except: 
+                print('Error downloading... Filename:')
+                print(file)
+
+print('Done downloading "dashboard" files')
 
 # %%
+### Vaccinations 
+url = "https://covid19.ssi.dk/overvagningsdata/download-fil-med-vaccinationsdata"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+links = soup.find_all("a", string=lambda text: "data" in str(text).lower())
+check_str_vacc = "<a href=\"https://files.ssi.dk/covid19/vaccinationsdata/zipfil/vaccinationsdata-dashboard"
+for link in links[3:]: 
+    # print('---')
+    # print(link)
+    if str(link)[:len(check_str_vacc)]!=check_str_vacc:
+        # print("not a file; continues...")
+        continue
+    # print(link)
+    file = link["href"]
+    # print(file)
+
+    # For all files after 2021-04-19, "-covid19-" is included in link
+    # To download earlier files, a different script should be made, in which something else is checked for in link...
+    covid19Pos = file.find('-covid19-')
+    curDay = file[covid19Pos+9:covid19Pos+11]
+    curMonth = file[covid19Pos+11:covid19Pos+13]
+    curYear = file[covid19Pos+13:covid19Pos+17]
+    curDate = f'{curYear}-{curMonth}-{curDay}'
+    curDateTime = np.datetime64(curDate)
+    # print(curDateTime)
+    
+    if download_all_data:
+
+        filename = "\\SSI_vacc_" + curDate
+        zipped_save_path = path_dash + "_zipped" + filename + ".zip"
+        extracted_save_path = path_dash + filename
+        print(f'Downloading: {extracted_save_path}')
+        
+        try:
+            download_url(file, zipped_save_path)
+            with zipfile.ZipFile(zipped_save_path, 'r') as zipObj:
+                zipObj.extractall(extracted_save_path)
+        except: 
+            print(file)
+    else:
+        if (curDateTime > mostRecentDate):
+            
+            filename = "\\SSI_vacc_" + curDate
+            zipped_save_path = path_dash + "_zipped" + filename + ".zip"
+            extracted_save_path = path_dash + filename
+            print(f'Downloading: {extracted_save_path}')
+            
+            try:
+                download_url(file, zipped_save_path)
+                with zipfile.ZipFile(zipped_save_path, 'r') as zipObj:
+                    zipObj.extractall(extracted_save_path)
+            except: 
+                print('Error downloading... Filename:')
+                print(file)
+
+print('Done downloading "vaccination" files')
+
+
