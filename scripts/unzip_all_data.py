99ea488e553592a38f2d56761517694f50f30fa9
Rasmus Kristoffer Pedersen <rasmuspedersen1992@gmail.com>
2022-01-17 12:45:29 +0100

    Made a script for unzipping all zipped data files

---
+++ scripts/unzip_all_data.py
@@ -0,0 +1,62 @@
+# %%
+# Script for unzipping all data
+import zipfile
+import io
+import os
+
+# Define paths
+rootdir_data = os.getcwd() +"\\..\\DanskeData\\" 
+
+path_data = rootdir_data + "ssi_data"
+path_dash = rootdir_data + "ssi_dashboard"
+path_vacc = rootdir_data + "ssi_vacc"
+
+# %%
+# Overvågningsdata
+curRoot = path_data
+curFiles = os.listdir(curRoot+'_zipped')
+
+for thisFile in curFiles:
+
+    filename = "\\" + thisFile[:-4]
+    zipped_save_path = curRoot + "_zipped" + filename + ".zip"
+    extracted_save_path = curRoot  + filename
+    # print(f'Extracting: {extracted_save_path}')
+    
+    with zipfile.ZipFile(zipped_save_path, 'r') as zipObj:
+        zipObj.extractall(extracted_save_path)
+print('Done extracting all "overvågningsdata" files')
+
+# %%
+# Dashboard
+curRoot = path_dash
+curFiles = os.listdir(curRoot+'_zipped')
+
+for thisFile in curFiles:
+
+    filename = "\\" + thisFile[:-4]
+    zipped_save_path = curRoot + "_zipped" + filename + ".zip"
+    extracted_save_path = curRoot  + filename
+    # print(f'Extracting: {extracted_save_path}')
+    
+    with zipfile.ZipFile(zipped_save_path, 'r') as zipObj:
+        zipObj.extractall(extracted_save_path)
+print('Done extracting all "dashboard" files')
+
+# %%
+# Vaccinations
+curRoot = path_vacc
+curFiles = os.listdir(curRoot+'_zipped')
+
+for thisFile in curFiles:
+
+    filename = "\\" + thisFile[:-4]
+    zipped_save_path = curRoot + "_zipped" + filename + ".zip"
+    extracted_save_path = curRoot  + filename
+    # print(f'Extracting: {extracted_save_path}')
+    
+    with zipfile.ZipFile(zipped_save_path, 'r') as zipObj:
+        zipObj.extractall(extracted_save_path)
+print('Done extracting all "vaccination" files')
+
+
