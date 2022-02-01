38621da4e908ad73df81ab37a1d4dc48862f903b
Rasmus Kristoffer Pedersen <rasmuspedersen1992@gmail.com>
2022-02-01 09:03:53 +0100

    Changed cumulative figures to show raw change instead of 7-day average. Also fixed Samsø

--- web/convertMarkdownToHTMLWindows_CurDir.bat
+++ web/convertMarkdownToHTMLWindows_CurDir.bat
@@ -2,4 +2,5 @@ ECHO Converting files to HTML using pandoc
 pandoc VinterSammenligning.md -o VinterSammenligning.html
 pandoc Regionalt.md -o Regionalt.html
 pandoc Aldersfordeling.md -o Aldersfordeling.html
+pandoc Kommunalt.md -o Kommunalt.html
 ECHO Done!
